e9e91cb71b939109d6f21078c0c1d00dca54eb0b
Aaron Davis <aaron.e.davis@gmail.com>
2022-05-26 15:38:32 -0600

    Added Cisco Sample Code License

--- sandbox/rule_cleanup.py
+++ sandbox/rule_cleanup.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
-"""
-This script:
+
+"""This script:
     1) retrieves L3 Firewall rules from the Meraki dashboard
     2) evaluates the rules for duplicates
     3) cleans up the duplicates
     4) uploads the sanitized ruleset back to the Meraki Dashboard.
 
-"""
-
 __author__ = "Aaron Davis"
-__version__ = "0.2.0"
-__copyright__ = "Copyright (c) 2022 Aaron Davis"
-__license__ = "MIT License"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2022 Cisco and/or its affiliates"
+__license__ = "CISCO SAMPLE CODE LICENSE"
+
+Copyright (c) 2022 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied."""
 
 import sys
 import json
